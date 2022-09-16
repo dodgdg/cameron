e297360c9c217562c9be045fdd40b43fa0d9e850
dodg <sam@mccardal.com>
2022-09-16 11:44:13 +0100

    added terminal display / interface

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
 extern crate termion;
 
-use termion::{color::{self, Color}, style};
 use std::mem::{size_of_val};
+use std::io::{Write, stdout, stdin};
+
+use termion::{color::{self, Color}, style};
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::IntoRawMode;
 
 const BOARD_WIDTH: usize = 7;
 const BOARD_HEIGHT: usize = 6;
 
 const CIRCLE: &str = "\u{2B24}";
-const PLAYER_1_COLOR_COLOR: color::Fg<color::Red> = color::Fg(color::Red);
+const PLAYER_1_COLOR: color::Fg<color::Red> = color::Fg(color::Red);
 const PLAYER_2_COLOR: color::Fg<color::Blue> = color::Fg(color::Blue);
+const DEFAULT_COLOR: color::Fg<color::White> = color::Fg(color::White);
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 enum Player {
@@ -25,7 +31,7 @@ impl Player {
     }
 }
 
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, PartialEq)]
 enum Counter {
     PlayerCounter(Player),
     NoCounter,
@@ -70,9 +76,51 @@ fn default_board() -> Board {
 }
 
 impl Board {
-    fn winning_move(&self, column: usize, row: usize) -> bool {
-        true  // need to do all the checks, efficiently!
-    }
+//     fn winning_move(&self, column: usize, row: usize) -> bool {
+//         let turn_player = Counter::PlayerCounter(self.turn);
+// //  SHIT you forgot that we could be completing the line from the middle also
+//         if row >= 3 {
+//             // vertical check (S)
+//             if self.matrix[column][row - 1] == turn_player
+//             && self.matrix[column][row - 2] == turn_player
+//             && self.matrix[column][row - 3] == turn_player {
+//                 return true;
+//             }
+//             // diagonal check SE
+//             if column <= BOARD_WIDTH - 4
+//             && self.matrix[column + 1][row - 1] == turn_player
+//             && self.matrix[column][row - 2] == turn_player
+//             && self.matrix[column][row - 3] == turn_player {
+//                 return true;
+//             }
+//             // diagonal check SW
+//             if column >= 3
+//             && self.matrix[column][row - 1] == turn_player
+//             && self.matrix[column][row - 2] == turn_player
+//             && self.matrix[column][row - 3] == turn_player {
+//                 return true;
+//             }
+
+//         if row <= BOARD_HEIGHT - 4 {
+//             // no vertical check (N)
+
+//             // diagonal check NE
+//             if column <= BOARD_WIDTH - 4
+//             && self.matrix[column + 1][row - 1] == turn_player
+//             && self.matrix[column][row - 2] == turn_player
+//             && self.matrix[column][row - 3] == turn_player {
+//                 return true;
+//             }
+//             // diagonal check NW
+//             if column >= 3
+//             && self.matrix[column][row - 1] == turn_player
+//             && self.matrix[column][row - 2] == turn_player
+//             && self.matrix[column][row - 3] == turn_player {
+//                 return true;
+//             }
+
+//         return false;
+//     }
 
     fn make_move(&mut self, player_move: PlayerMove) -> Result<&Board, MoveError> {
         if self.winner != Winner::NoWinner {
@@ -91,32 +139,68 @@ impl Board {
             return Err(MoveError::ColumnFull);
         }
         
-        if self.winning_move(player_move.column, spot) {
-            self.winner = Winner::WinningPlayer(player_move.player);
-        }
+        // if self.winning_move(player_move.column, spot) {
+            // self.winner = Winner::WinningPlayer(player_move.player);
+        // }
         self.matrix[player_move.column][spot] = Counter::PlayerCounter(player_move.player);
         self.top_spot[player_move.column] += 1;
         self.turn = self.turn.other();
 
         Ok(self)
     }
+
+    fn display(&self) -> String {
+        let mut board_string = String::from("");
+        for y in 0..BOARD_HEIGHT {
+            for x in 0..BOARD_WIDTH {
+                match self.matrix[x][BOARD_HEIGHT - 1 - y] {
+                    Counter::PlayerCounter(Player::Player1) => board_string.push_str(&format!("|{}{}{} ", PLAYER_1_COLOR, CIRCLE, DEFAULT_COLOR)),
+                    Counter::PlayerCounter(Player::Player2) => board_string.push_str(&format!("|{}{}{} ", PLAYER_2_COLOR, CIRCLE, DEFAULT_COLOR)),
+                    Counter::NoCounter => board_string.push_str("|  "),
+                }
+            }
+            board_string.push_str("|\r\n");
+        }
+        return board_string
+    }
 }
 
 fn main() {
+    let mut stdout = stdout().into_raw_mode().unwrap();
+
     let mut board = default_board();
-    println!("{:?}", board.make_move(PlayerMove { player: Player::Player1, column: 4 }).unwrap());
-    println!("{:?}", board.make_move(PlayerMove { player: Player::Player2, column: 4 }).unwrap());
+    
+    loop {
+        let stdin = stdin();
+        write!(stdout,
+            "{}\r\n{}{}{}",
+            termion::clear::All,
+            board.display(),
+            termion::cursor::Goto(1, 1),
+            termion::cursor::Hide
+        )
+        .unwrap();
+        stdout.flush().unwrap();
 
-    println!("size of board: {}", size_of_val(&board));
-    println!("size of matrix: {}", size_of_val(&board.matrix));
-    println!("size of turn: {}", size_of_val(&board.turn));
-    println!("size of top spot: {}", size_of_val(&board.top_spot));
+        for c in stdin.keys() {
+            write!(stdout,
+                "{}{}",
+                termion::cursor::Goto(1, 1),
+                termion::clear::CurrentLine)
+                    .unwrap();
 
-    let x: u8 = 4;
-    let y = Counter::PlayerCounter(Player::Player1);
-
-    println!("size of u8: {}", size_of_val(&x));
-    println!("size of counter: {}", size_of_val(&y));
-
-    println!("Meh");
+            match c.unwrap() {
+                Key::Char(x) => {
+                    if let Some(d) = x.to_digit(10) {
+                        if d > 0 && d < (BOARD_WIDTH + 1) as u32 {
+                            board.make_move(PlayerMove { player: board.turn, column: (d - 1) as usize }).unwrap();
+                            break;
+                        }
+                    }
+                }
+                _ => {}
+            }
+            stdout.flush().unwrap();
+        }
+    }
 }
